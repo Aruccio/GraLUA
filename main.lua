f7aa4af53d0a00065e3b0f37b78ec1cee6b0b358
Aruccio <arleni@o2.pl>
2020-11-09 13:12:26 +0100

    start

---
+++ main.lua
@@ -0,0 +1,3 @@
+local background = display.newImageRect("trawa.jpg", display.actualContentWidth,display.actualContentHeight)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
